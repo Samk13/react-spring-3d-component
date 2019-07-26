15ee2f3afe020a18a07557867fbc841fa6ab28cc
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2019-07-26 23:04:13 +0200

    update Components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { useSpring, animated } from 'react-spring'
 import './styles.css'
+import Card from './Components/Component3D'
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
-
-function Card() {
-  const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
-  return (
-    <animated.div
-      class="card"
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-      onMouseLeave={() => set({ xys: [0, 0, 1] })}
-      style={{ transform: props.xys.interpolate(trans) }}
-    />
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Card />
+      </div>
+    )
+  }
 }
 
-ReactDOM.render(<Card />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
